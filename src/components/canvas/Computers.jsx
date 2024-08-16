c8df17783a3d913046a3f1a23cd710325c7c58a7
plusquam <erwinramos17@gmail.com>
2024-08-16 14:15:48 -0600

    Creation project basis

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,55 @@
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <ambientLight intensity={1} />
+      <spotLight
+        position={[-3, 5, 1]}
+        angle={1}
+        penumbra={1}
+        intensity={5}
+        castShadow
+        shadow-mapSize={1024}
+        decay={false}
+      />
+
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[1, -3, -1.5]}
+        receiveShadow
+        // rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 0, 25], fov: 15 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
